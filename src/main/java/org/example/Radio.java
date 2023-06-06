a1fcf32bfe6e26f60ed5bf7cd8238d9f6659c4ae
Evgeniy Zlobina <ZEA_6590@mail.ru>
2023-06-06 21:38:54 +0300

    Designer

--- src/main/java/org/example/Radio.java
+++ src/main/java/org/example/Radio.java
@@ -4,18 +4,28 @@ public class Radio {
     private int radioStationNumber;
     private int currentVolume;
 
+    public int quantity = 9;
+
+    //if (radioStationNunber = quantity)
+    public Radio() {
+      this.quantity = quantity;
+    }
+
+    public Radio (int quantity) {
+        this.quantity = quantity-1;
+    }
+
     public int getNextradioStationNumber() {
 
         return radioStationNumber;
     }
 
-    // установка номера радиостанции
     public void setNextradioStationNumber(int newNextradioStationNumber) {
         if (newNextradioStationNumber < 0) {
             return;
         }
-        if (newNextradioStationNumber > 9) {
-            return;
+        if (newNextradioStationNumber > quantity) {
+            newNextradioStationNumber = quantity;
         }
         radioStationNumber = newNextradioStationNumber;
     }
@@ -28,32 +38,30 @@ public class Radio {
     // установка громкости звука
     public void setCurrentVolume(int newcurrentVolume) {
 
-        // if (newcurrentVolume > 100) {
-        //    return;
-        //}
         if (newcurrentVolume < 0) {
             return;
         }
         currentVolume = newcurrentVolume;
     }
 
+    // установка номера радиостанции
     public void prewradio() {
         int nomerradio;
-        //radioStationNumber = new radioStationNumber;
+
         if (radioStationNumber == 0) {
-            nomerradio = 9;
-            // setNextradioStationNumber(nomerradio);
+            nomerradio = quantity;
+
         } else {
             nomerradio = radioStationNumber - 1;
-            //  setNextradioStationNumber(nomerradio);
+
         }
         setNextradioStationNumber(nomerradio);
     }
 
     public void nextradio() {
         int nomerradio;
-        // radioStationNumber = newradioStationNumber;
-        if (radioStationNumber == 9) {
+
+        if (radioStationNumber == quantity) {
             nomerradio = 0;
             radioStationNumber = nomerradio;
         } else {
@@ -62,6 +70,7 @@ public class Radio {
         }
     }
 
+    // увеличение громкости звука
     public void increasevolume() {
         int volume;
         if (currentVolume < 100) {
